3ccc3613383127b63ebe31f5c7d0f173b419d912
Victor Sanchez <victorssanchez@hotmail.es>
2024-08-18 12:24:47 +0200

    Creados componentes list y addCharacter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterModule } from './contador/counter.module';
 import { HeroModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { HeroModule } from './heroes/heroes.module';
     AppRoutingModule,
     CounterModule,
     HeroModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
