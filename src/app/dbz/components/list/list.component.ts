04dbceb04de794a5c8c66e1163b8f60e38642dfb
VICTOR SANCHEZ <victorssanchez@hotmail.es>
2024-08-19 19:54:20 +0200

    docs creado

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'dbz-list',
@@ -10,8 +11,19 @@ export class ListComponent {
 
   @Input()
   public listCharacter:Character[]=[{
+    id: uuid(),
     name:'Trunks',
     power:10
   }
   ];
+
+  @Output()
+  public onDelete:EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?:string):void{
+
+    if(!id) return;
+
+    this.onDelete.emit(id);
+  }
 }
--- src/app/dbz/interfaces/character.interface.ts
+++ src/app/dbz/interfaces/character.interface.ts
@@ -1,6 +1,7 @@
 
 
 export interface Character {
-  name:string;
-  power:number;
+  id?    :string;
+  name  :string;
+  power :number;
 }
